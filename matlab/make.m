ab131e8f3a561dc9f12312f17d8482ab2744bf53
Stefano Sinigardi <stesinigardi@hotmail.com>
2017-06-06 11:03:33 +0200

    move output mex files to appropriate folder (we use libsvm as a git submodule)

--- matlab/make.m
+++ matlab/make.m
@@ -3,17 +3,17 @@ function make()
 try
 	% This part is for OCTAVE
 	if (exist ('OCTAVE_VERSION', 'builtin'))
-		mex libsvmread.c
-		mex libsvmwrite.c
-		mex -I.. svmtrain.c ../svm.cpp svm_model_matlab.c
-		mex -I.. svmpredict.c ../svm.cpp svm_model_matlab.c
+		mex -o ../../libsvmread libsvmread.c
+		mex -o ../../libsvmwrite libsvmwrite.c
+		mex -o ../../svmtrain -I.. svmtrain.c ../svm.cpp svm_model_matlab.c
+		mex -o ../../svmpredict -I.. svmpredict.c ../svm.cpp svm_model_matlab.c
 	% This part is for MATLAB
 	% Add -largeArrayDims on 64-bit machines of MATLAB
 	else
-		mex CFLAGS="\$CFLAGS -std=c99" -largeArrayDims libsvmread.c
-		mex CFLAGS="\$CFLAGS -std=c99" -largeArrayDims libsvmwrite.c
-		mex CFLAGS="\$CFLAGS -std=c99" -I.. -largeArrayDims svmtrain.c ../svm.cpp svm_model_matlab.c
-		mex CFLAGS="\$CFLAGS -std=c99" -I.. -largeArrayDims svmpredict.c ../svm.cpp svm_model_matlab.c
+		mex -output ../../libsvmread CFLAGS="\$CFLAGS -std=c99" -largeArrayDims libsvmread.c
+		mex -output ../../libsvmwrite CFLAGS="\$CFLAGS -std=c99" -largeArrayDims libsvmwrite.c
+		mex -output ../../svmtrain CFLAGS="\$CFLAGS -std=c99" -I.. -largeArrayDims svmtrain.c ../svm.cpp svm_model_matlab.c
+		mex -output ../../svmpredict CFLAGS="\$CFLAGS -std=c99" -I.. -largeArrayDims svmpredict.c ../svm.cpp svm_model_matlab.c
 	end
 catch err
 	fprintf('Error: %s failed (line %d)\n', err.stack(1).file, err.stack(1).line);
